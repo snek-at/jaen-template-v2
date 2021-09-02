8469fd8f7b3586d2cec5c11bdd434c9c54959776
Florian Kleber <kleberbaum@erebos.xyz>
2021-09-02 16:08:08 +0000

    chore: update versions

---
+++ gatsby-config.js
@@ -0,0 +1,48 @@
+const path = require("path")
+require('source-map-support').install()
+require('ts-node').register({compilerOptions: {esModuleInterop: true}})
+
+module.exports = {
+  siteMetadata: {
+    title: "Jason’s Blog Theme — It’s SICK",
+    description: `
+      This is a blog theme. The description will be showed in SEO results on pages
+      without their own descriptions.
+    `,
+    siteUrl: "https://example.com",
+    image: "https://lengstorf.com/images/jason-lengstorf.jpg",
+    author: {
+      name: "Your Name",
+      minibio: `
+        This bio is shown at the bottom of each blog post. It supports
+        <strong>custom HTML</strong> if you’re into that sort of thing.
+      `,
+    },
+    organization: {
+      name: "Example, Inc.",
+      url: "https://example.com",
+      logo: "https://lengstorf.com/android-chrome-512x512.png",
+    },
+    social: {
+      twitter: "@jlengstorf",
+      fbAppID: "",
+    },
+    categories: [
+      {
+        slug: "test",
+        name: "Test Category",
+      },
+    ],
+  },
+  plugins: [
+    "@snek-at/jaen",
+    {
+      resolve: "@snek-at/jaen-pages",
+      options: {
+        templates: {
+          SamplePage: path.resolve("src/templates/SamplePage.tsx"),
+        },
+      },
+    },
+  ],
+}
