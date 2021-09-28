35da09367cb96f253214857afa501acd26587447
petute <daniel.petutschnigg@gmail.com>
2021-09-28 02:16:19 +0200

    test6

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import {Box, Heading, Center} from '@chakra-ui/react'
+
+const IndexPage = () => {
+  return (
+    <Box>
+      <Center mt="45vh">
+        <Heading>Hello World</Heading>
+      </Center>
+    </Box>
+  )
+}
+//#endregion
+
+//#region > Exports
+export default IndexPage
+//#endregion
